ff655606913db1c2d715eac66526262ca19e060e
Erik Lucas Da Rocha <e.lucasrocha@gmail.com>
2022-06-16 09:15:42 -0300

    Day 9 finished!

---
+++ Day 9 - Recursion/recursion.py
@@ -0,0 +1,13 @@
+def factorial(n):
+    # Write your code here
+    if n == 1:
+        return 1
+    else:
+        return(n * factorial(n-1))
+
+n = int(input().strip())
+
+result = factorial(n)
+
+print(result)
+
